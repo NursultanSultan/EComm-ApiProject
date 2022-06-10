c014eabe4d06a09809b311730eb7168fced6deed
NursultanSultan <tu201906251@code.edu.az>
2022-06-11 01:09:24 +0400

    Add project files.

---
+++ Infrastructure/ECommerseApi.Persistence/ServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerseApi.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection service)
+        {
+
+        }
+    }
+}

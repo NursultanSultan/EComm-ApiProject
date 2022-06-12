28d63b95db2f94c304d1612611fd4e08c2b20f64
NursultanSultan <tu201906251@code.edu.az>
2022-06-12 16:37:31 +0400

    'refactor : Interceptor optimization'

--- Infrastructure/ECommerseApi.Persistence/Contexts/ApiDbContext.cs
+++ Infrastructure/ECommerseApi.Persistence/Contexts/ApiDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerseApi.Domain.Entities;
+using ECommerseApi.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -14,5 +15,22 @@ namespace ECommerseApi.Persistence.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow
+                };
+            }
+
+
+            return base.SaveChangesAsync(cancellationToken);    
+        }
     }
 }
--- Presentation/ECommerseApi.Api/Controllers/ProductController.cs
+++ Presentation/ECommerseApi.Api/Controllers/ProductController.cs
@@ -22,15 +22,7 @@ namespace ECommerseApi.Api.Controllers
         [HttpGet]
         public async Task Get()
         {
-            //await _productWriteRepository.AddRangeAsync(new()
-            //{
-            //    new(){Id = Guid.NewGuid(),Name = "Salam",Price=999,Stock=10  , CreatedDate = DateTime.UtcNow},
-            //    new(){Id = Guid.NewGuid(),Name = "Aleykum",Price=888,Stock=20  , CreatedDate = DateTime.UtcNow}
-            //});
-
-            //Product pr = await _productReadRepository.GetByIdAsync("eee6b33e-1be1-48ab-9175-df9cf07be09f",false);
-            //pr.Name = "Alalala";
-            //await _productWriteRepository.SaveChangeAsync();
+           
         }
     }
 }

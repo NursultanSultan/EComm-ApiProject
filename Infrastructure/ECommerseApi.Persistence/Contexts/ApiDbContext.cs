5451377da3910c88285a0706e69ad21178a594e4
NursultanSultan <tu201906251@code.edu.az>
2022-06-19 16:19:40 +0400

    'fix : SaveChangeAsync function fixed'

--- Infrastructure/ECommerseApi.Persistence/Contexts/ApiDbContext.cs
+++ Infrastructure/ECommerseApi.Persistence/Contexts/ApiDbContext.cs
@@ -26,7 +26,7 @@ namespace ECommerseApi.Persistence.Contexts
                 {
                     EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
                     EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow,
-                    
+                    _ => DateTime.UtcNow
                 };
             }
 
